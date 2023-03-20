6764f9ebc2cb8d39912f355897310583773ba1e0
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-03-20 10:33:19 -0500

    added sidebar and taskarea component

---
+++ todo-react/src/components/taskArea/taskArea.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactElement } from 'react';
+
+import { Grid } from '@mui/material';
+
+
+export const TaskArea: FC = (): ReactElement => {
+    return(
+        <Grid item md={8} px={4}>
+                <h2>Content area</h2>
+        </Grid>
+    )
+}
